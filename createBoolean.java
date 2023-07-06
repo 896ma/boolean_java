6ec0dfe32ae62c68721d6d7fe7516cc75de45247
896ma <solarcrash3@gmail.com>
2023-07-07 00:25:50 +0300

    javabool

---
+++ createBoolean.java
@@ -0,0 +1,11 @@
+class createBoolean{
+    public static void main(String args []){
+        boolean kunacatkesho =true;
+        boolean itakufinya =false;
+        
+        System.out.println(kunacatkesho);
+          System.out.println(itakufinya);
+        
+        
+}
+} 
